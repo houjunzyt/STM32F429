e18cb9f3f07da066255d94baa15714f516ce33cc
houjunzyt <1562335917@qq.com>
2019-01-30 15:41:31 +0800

    set debug usart1 and init extern SDRAM.

--- User/main.c
+++ User/main.c
@@ -1,14 +1,26 @@
 #include "stm32f4xx.h"
 #include "delay.h"
 #include "led.h"
-
+#include "sdram.h"
+#include "usart.h"
 
 int main(void)
 {
 	SysTick_Config(SystemCoreClock/100000);//us级别定时
+	Usart1_Config();
+	SDRAM_Init();
 	LED_Init();
+	if(SDRAM_Test())
+	{
+		printf("SDRAM init Success\n");
+	}
+	else
+	{
+		printf("SDRAM init fail\n");
+	}
   while(1)
 	{
+		printf("kaka\n");
 		LED1_ON;
 		LED2_ON;
 		Delay_ms(1000);
