2ac2d03aff39634482730b6d75f2dc085717109a
houjunzyt <1562335917@qq.com>
2019-02-13 16:28:03 +0800

    add touch info funcation

--- User/main.c
+++ User/main.c
@@ -5,9 +5,10 @@
 #include "usart.h"
 #include "touch_043.h"
 #include "touch_iic.h"
+#include "lcd_rgb.h"
 
 int i __EXRAM;
-
+void LCD_AlphaTest(void);
 int main(void)
 {
 	SysTick_Config(SystemCoreClock/100000);//us级别定时
@@ -15,7 +16,7 @@ int main(void)
 	SDRAM_Init();//SDRAM初始化
 	LED_Init();//板载LED灯初始化
 	Touch_Init();		//触摸屏初始化
-	
+	LCD_Init();			// LCD控制器初始化
 	if(SDRAM_Test())
 	{
 		printf("SDRAM init Success\n");
@@ -24,16 +25,9 @@ int main(void)
 	{
 		printf("SDRAM init fail\n");
 	}
-	char * kk=malloc(1024*sizeof(char));
-	for(i=0;i<1024;i++)
-	{
-		kk[i]=i;
-	}
-	printf("%d :%d\n",5,kk[5]);
-	printf("%d :%d\n",200,kk[200]);
-	printf("kaka:%x\n",(int)&i);
   while(1)
 	{
+		LCD_AlphaTest();	//透明度测试
 		Touch_Scan(); //触摸扫描
 		if(touchInfo.flag==1)		// 若发生触摸
 		{
@@ -43,4 +37,44 @@ int main(void)
 }
 
 
+void LCD_AlphaTest(void)
+{
+	LCD_DisplayMode(Mode_H);	//设置横屏
+	LCD_SetFont(&Font24);		//设置字体
 
+	LCD_SetLayer(0);		//切换到层0	
+	LCD_SetColor(LCD_BLACK);	
+	LCD_SetBackColor(LIGHT_BLUE);   
+	LCD_Clear();
+	LCD_DisplayString(10,20,"  STM32F429 LCD Layer 0"); 
+
+	LCD_SetLayer(1);	//切换到层1
+	LCD_SetBackColor(LIGHT_YELLOW & 0x00FFFFFF);  //透明度100%，高8位的数字越小透明度越高
+	LCD_Clear();											 //此时前景层完全透明
+
+	LCD_SetColor(LCD_BLACK & 0x0DFFFFFF);		
+	LCD_SetBackColor(LIGHT_YELLOW & 0x0DFFFFFF);  	
+	LCD_DisplayString(10,60," STM32F429 LCD Layer 1,Alpha:95%  "); 
+
+	LCD_SetColor(LCD_BLACK & 0x33FFFFFF);			
+	LCD_SetBackColor(LIGHT_YELLOW & 0x33FFFFFF); 
+	LCD_DisplayString(10,90," STM32F429 LCD Layer 1,Alpha:80%  "); 
+
+	LCD_SetColor(LCD_BLACK & 0x66FFFFFF);
+	LCD_SetBackColor(LIGHT_YELLOW & 0x66FFFFFF); 
+	LCD_DisplayString(10,120," STM32F429 LCD Layer 1,Alpha:60%  "); 
+
+	LCD_SetColor(LCD_BLACK & 0x99FFFFFF);
+	LCD_SetBackColor(LIGHT_YELLOW & 0x99FFFFFF); 
+	LCD_DisplayString(10,150," STM32F429 LCD Layer 1,Alpha:40%  "); 
+
+	LCD_SetColor(LCD_BLACK & 0xccFFFFFF);
+	LCD_SetBackColor(LIGHT_YELLOW & 0xCCFFFFFF); 
+	LCD_DisplayString(10,180," STM32F429 LCD Layer 1,Alpha:20%  "); 
+
+	LCD_SetColor(LCD_BLACK & 0xffFFFFFF);
+	LCD_SetBackColor(LIGHT_YELLOW & 0xFFFFFFFF); 
+	LCD_DisplayString(10,210," STM32F429 LCD Layer 1,Alpha: 0%  "); 
+
+	Delay_ms(1000);
+}
